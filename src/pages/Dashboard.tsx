dd31fe82d15b045522198e3065727d7e25c42385
tejas122125 <tejasweekumarsingh@gmail.com>
2024-03-09 20:19:40 +0530

    tejas

--- src/pages/Dashboard.tsx
+++ src/pages/Dashboard.tsx
@@ -10,7 +10,7 @@ const Dashboard = () => {
     return (
         <div className='w-screen h-screen bg-black  gap-4 p-4 overflow-y-auto'>Dashboard
             <Link to='/'>sfreferf</Link>
-            <div className='h-[800px] p-4 bg-purple-400'>
+            <div className='h-[800px] p-4 '>
                 <ResizablePanelGroup direction="vertical">
                     <ResizablePanel className='p-1'>
                         <ResizablePanelGroup direction="horizontal">
@@ -24,7 +24,7 @@ const Dashboard = () => {
                         <ResizablePanelGroup direction="horizontal">
                             <ResizablePanel className='bg-red-400 p-2'><iframe src="http://localhost:3000/d-solo/CgCw8jKZz/go-metrics?orgId=1&refresh=5s&panelId=14" className='w-full h-full'></iframe></ResizablePanel>
                             <ResizableHandle withHandle />
-                            <ResizablePanel className='bg-orange-400 p-2'>Two</ResizablePanel>
+                            <ResizablePanel className='bg-orange-400 p-2'><iframe src="http://localhost:3000/d-solo/CgCw8jKZz/go-metrics?orgId=1&refresh=5s&panelId=20" className='w-full h-full'   ></iframe></ResizablePanel>
                         </ResizablePanelGroup>
                     </ResizablePanel>
                 </ResizablePanelGroup>
